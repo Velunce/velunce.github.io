e147b0d0a1b00c33a89962927de5c02b66a51b23
Velunce Zhang <velunce@Velunces-MacBook-Pro.local>
2018-07-04 21:55:17 +0800

    Merge site code

---
+++ js/back.js
@@ -0,0 +1,217 @@
+< !--PORTFOLIO PAGE-- >
+  <li id="portfolio" class="hidden">
+    <div class="title-container">
+      <div class="shadow-img"></div>
+      <h2 class="rotate-out rotated">Collections</h2>
+      <!--PORTFOLIO TITLE-->
+                        </div>
+    <!--PORTFOLIO CATEGORY LIST-->
+                        <div class="abs-child">
+      <ul class="button-group filters-button-group">
+        <li class="button is-checked" data-filter="*">Show All</li>
+        <li class="button" data-filter=".photography">Photography</li>
+        <li class="button" data-filter=".commercial">Commercial</li>
+        <li class="button" data-filter=".Dashboard">Dashboard</li>
+      </ul>
+      <!--/.button-group filters-button-group-->
+                        </div>
+    <div class="sec-porfolio description">
+      <div class="row">
+        <!--PORTFOLIO ITEM CONTAINER-->
+                                <div class="folio-container">
+          <ul class="folio-item">
+            <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                        <li class="view view-first photography">
+              <img src="images/folio/logs.jpg" alt="logs" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask">
+                <h2>woodsman</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <!-- Button trigger modal -->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-second commercial">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/book.jpg" alt="book" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask"></div>
+              <div class="content">
+                <h2>Hover Style #2</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal2">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-tenth photography">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/beach.jpg" alt="beach" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask">
+                <h2>New York</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal5">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-second commercial">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/guy.jpg" alt="guy" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask"></div>
+              <div class="content">
+                <h2>Image Name</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal3">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-first Dashboard">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/illustration.jpg" alt="illustration" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask">
+                <h2>New York</h2>
+                <a role="button" class="info" data-toggle="modal" data-target="#myModal4">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-second photography">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/guy.jpg" alt="guy" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask"></div>
+              <div class="content">
+                <h2>Image Name</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal3">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-tenth Dashboard">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/logs.jpg" alt="logs" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask">
+                <h2>woodsman</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+            <li class="view view-tenth commercial">
+              <!--PORTFOLIO INDIVIDAUAL ITEM-->
+                                            <img src="images/folio/beach.jpg" alt="beach" />
+              <!--PORTFOLIO INDIVIDAUAL ITEM IMAGE-->
+                                            <div class="mask">
+                <h2>New York</h2>
+                <!--PORTFOLIO INDIVIDAUAL ITEM TITLE-->
+                                                <a role="button" class="info" data-toggle="modal" data-target="#myModal5">Read More</a>
+                <!--PORTFOLIO INDIVIDAUAL ITEM MODAL LINK BY 'data-target'-->
+                                            </div>
+            </li>
+          </ul>
+        </div>
+        <!--PORTFOLIO ITEM CONTAINER ENDS-->
+                            </div>
+    </div>
+  </li>
+  <!--/#portfolio-->
+
+    < li id = "employment" class="hidden" >
+      <div class='tabs tabs_animate'>
+        <ul class='horizontal no-padding'>
+          <li>
+            <a href="#tab-3" class="icon-bg icon-employment">
+              <div>Employment</div>
+            </a>
+          </li>
+        </ul>
+        <div id='tab-3'>
+          <h3 class="title">EMPLOYMENT</h3>
+          <ul class="employment-class tab-cont">
+            <li>
+              <!--EMPLOYMENT INDIVIDUAL LIST-->
+                                        <h4>Front-End Developer
+                                            <span class="year">2015 - NOW</span>
+              </h4>
+              <h5>Avanade Ltd</h5>
+              <p>Use HTML5 related new technology to serve as front-end engineer in SAP HANA, CIO
+                  MANAGEMENT RECORDS, Huawei Finance cloud radar, SEC Shanghai power purchase project.
+                  All the hands of the project are delivered in advance, part of the project during
+                  the second development process through a large number of H5 semantic label reconstruction
+                  page, many applications H5 new features. On the line after the interactive effect
+                  of the company and customers praise (the above items for internal projects, can
+                                            not provide access to links).</p>
+              <!--EMPLOYMENT LIST DETAILS-->
+                                    </li>
+            <li>
+              <!--EMPLOYMENT INDIVIDUAL LIST-->
+                                        <h4>Front-End Developer
+                                            <span class="year">2014 - 2005</span>
+              </h4>
+              <h5>Shenyang Taiji Culture Media Co., Ltd</h5>
+              <p>During the working period to lead more than 3 front-end development team, the development
+                  and production of H5 mobile site, WEB APP, WeChat mobile phone application of
+                  a variety of animation site, the development of the company front-end development
+                  standards. According to customer requirements finishing a complete set of standard
+                  CSS3 animation effect files applied to all the company H5 pages (mobile phone
+                  side website example). Independent production, transformation of various types
+                  of H5 games, according to customer needs for customers customized mobile phone
+                  activities
+                                        </p>
+              <!--EMPLOYMENT LIST DETAILS-->
+                                    </li>
+            <li>
+              <!--EMPLOYMENT INDIVIDUAL LIST-->
+                                        <h4>Front-End Developer
+                                            <span class="year">2013 - 2014</span>
+              </h4>
+              <h5>Shenyang Ming Shihua Wo Technology Co., Ltd</h5>
+              <p>Led the 5-person front-end team, the application of H5 new technology development
+                  online O2O ordering platform. Use AXURE to make website prototypes. Use the METRO-UI
+                  style framework to build the front-end web interface. To achieve dynamic drag
+                  and drop, simulation AERO effects. Using CANVAS to achieve three - dimensional
+                  point dining table effects. Use geo-targeting to quickly match nearby businesses
+                  and enhance your users' physical exams. Responsible for front-end technical training,
+                  technical updates, with the background developers to solve various problems.
+                  (The project has been changed from the online platform to the terminal system,
+                                            no access to links)</p>
+              <!--EMPLOYMENT LIST DETAILS-->
+                                    </li>
+          </ul>
+        </div>
+      </div>
+                    </li >
+
+
+<ul class="education-class">
+                                        <li>
+                                            <!--EDUCATION LIST ITEM-->
+                                            <h4 class="title">University
+                                                <i class="fa fa-sort-desc pull-right"></i>
+                                            </h4>
+                                            <div class="list-content">
+                                                <h5 class="title">Dalian University of Technology
+                                                    <span class="list-year">(Mar, 2014 - Sep, 2016)</span>
+                                                </h5>
+                                                <p>Majoring in Network Engineering.</p>
+                                                <p>Get Excellent Graduates Certificate.</p>
+                                                <!--EDUCATION LIST DETAILS-->
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <!--EDUCATION LIST ITEM-->
+                                            <h4 class="title">College
+                                                <i class="fa fa-sort-desc pull-right"></i>
+                                            </h4>
+                                            <div class="list-content">
+                                                <h5 class="title">Dalian Vocational Technical College
+                                                    <span class="list-year">(Sep, 2010 - July, 2013)</span>
+                                                </h5>
+                                                <p>Majoring in Software Engineering</p>
+                                                <!--EDUCATION LIST DETAILS-->
+                                            </div>
+                                        </li>
+                                    </ul>
